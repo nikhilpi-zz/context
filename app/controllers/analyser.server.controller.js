a200dc879cddc97d053c8d5c7ada029721cc5f41
nikhilpi <nikhilpai2016@u.northwestern.edu>
2014-10-21 16:20:30 -0500

    getting keywords

--- app/controllers/analyser.server.controller.js
+++ app/controllers/analyser.server.controller.js
@@ -6,10 +6,15 @@
 var mongoose = require('mongoose'),
     _ = require('lodash');
 
-
+var AlchemyAPI = require('alchemy-api');
+var alchemy = new AlchemyAPI('2e76bbc542a2cc763bb5f2ffc0728850eb6c5c0d');
 
 exports.getKeywords = function(req, res) {
-  console.log('hello');
-  res.jsonp({'hello': 'world'});
+  var keywords = [];
+  alchemy.keywords(req.query.content, 'text',function(err, response) {
+    if (err) throw err;
+    keywords = response.keywords;
+    res.jsonp({'keywords': keywords});
+  });
 };
 
--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -3,14 +3,13 @@
 
 angular.module('core').controller('HomeController', ['$scope', '$http',
 	function($scope, $http) {
-    $scope.hello = 'hi';
-
     $scope.getKeywords = function(text){
       // Simple GET request example :
       console.log('getting keywords');
-      $http.get('/getKeywords', {content: text}).
+      $http.get('/getKeywords', {params: {content: text}}).
         success(function(response) {
           console.log(response);
+          $scope.keys = response.keywords;
         }).
         error(function(response) {
           console.log(response);
