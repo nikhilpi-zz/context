b9c66861a910897b34492448ec7aeb9c0f73ba3d
nikhilpi <nikhilpai2016@u.northwestern.edu>
2014-11-04 14:25:31 -0600

    keyword selection and custom adds

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -6,22 +6,35 @@ angular.module('core').controller('HomeController', ['$scope', 'Keywords', 'Type
     $scope.article = {
       text: 'lorem ipsum',
       keywords: []
-      types: []
+    };
+
+    $scope.addKeyword = function(keyword, relevance, selected){
+      relevance = relevance || 1;
+      selected = selected || false;
+      var newKeyword = {};
+      newKeyword.text = keyword;
+      newKeyword.relevance = relevance;
+      newKeyword.selected = selected;
+      $scope.article.keywords.push(newKeyword);
     };
 
     $scope.getKeywords = function() {
       var article = $scope.article;
       Keywords.get({content: article.text}, function(res) {
-        article.keywords = res.keywords;
+        var keys = res.keywords;
+        for (var i=0; i<keys.length; i++){
+          console.log(keys[i]);
+          $scope.addKeyword(keys[i].text, keys[i].relevance);
+        }
       });
     };
 
-    $scope.getTypes = function() {
-      var article = $scope.article;
-      Types.get({keywords: article.keywords}, function(res) {
-        console.log(res);
-      });
-    };
+    // $scope.getTypes = function() {
+    //   var article = $scope.article;
+    //   Types.get({keywords: article.keywords}, function(res) {
+    //     console.log(res);
+    //   });
+    // };
 
   
 	}
